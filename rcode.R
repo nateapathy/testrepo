8caac22939290f72ec468f1b3990429b2e2bb9e8
nateapathy <nate.apathy@gmail.com>
2016-10-20 23:34:52 -0400

    data import and histograms

--- rcode.R
+++ rcode.R
@@ -1,2 +1,12 @@
-#R test script
-#edits to r script, second commit
+#Import hmresp file into data frame
+#Step1: Make sure you have pulled down the latest version of the repo
+#Step2: Create data frame for hmresp file
+hmresp <- read.csv(~/LOCALFILEPATH/testrepo/hmresp.csv)
+View (hmresp)
+hist(hmresp$BMI)
+hist(hmresp$Mental_State)
+hist(hmresp$Mental_State, 
+     main="Histogram for Mental State Rating", 
+     xlab="Mental State Rating out of 10", 
+     border="black", 
+     col="blue")
